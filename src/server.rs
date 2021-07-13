99b229f029b3e104d095f89e80dba0620665c586
Konstantin <drpdrp@mail.ru>
2021-07-14 00:22:40 +0300

    CHG: server.rs: reading data from the stream

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server{
     addr:String,
@@ -12,6 +13,16 @@ impl Server{
     pub fn run(self){
         println!("Listening on {}", self.addr);
 
-        let listener = TcpListener::bind(&self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept(){
+                Ok((mut stream, _))=>{
+                    let mut buffer = [0; 1024];
+                    stream.read(&mut buffer);
+                },
+                Err(e) => {println!("Failed to establish a connection: {}", e);}
+            }
+        }
     }
 }
