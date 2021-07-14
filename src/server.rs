b5f6b0007b86944700eadc2de6cc3887eae176b9
Konstantin <drpdrp@mail.ru>
2021-07-15 00:36:56 +0300

    ADD: a beginning of conversion trait

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server{
     addr:String,
@@ -19,7 +22,20 @@ impl Server{
             match listener.accept(){
                 Ok((mut stream, _))=>{
                     let mut buffer = [0; 1024];
-                    stream.read(&mut buffer);
+                    match stream.read(&mut buffer){
+                        Ok(_)=>{
+                            println!("Received a request: {}", 
+                                String::from_utf8_lossy(& buffer));
+
+                                match Request::try_from(&buffer[..]){
+                                    Ok(_) => {},
+                                    Err(e) => println!("Failed to parse a request : {}", e)
+                                }
+                        },
+                        Err(e)=>{
+                            println!("Failed to read from connecion {}", e);
+                        }
+                    };
                 },
                 Err(e) => {println!("Failed to establish a connection: {}", e);}
             }
