ccbd444308a14330c2aa6304707fe575bd394548
Konstantin <drpdrp@mail.ru>
2021-07-18 23:21:34 +0300

    ADD: query string representation as QueryString

--- src/http/query_string.rs
+++ src/http/query_string.rs
@@ -22,3 +22,32 @@ impl<'buf> QueryString<'buf>{
         self.data.get(key)
     }
 }
+
+impl<'buf> From<&'buf str> for QueryString<'buf>{
+    fn from(s: &'buf str) -> Self{
+        let mut data = HashMap::new();
+
+        for sub_str in s.split('&'){
+            let mut key = sub_str;
+            let mut val = "";
+            if let Some(i) = sub_str.find('='){
+                key = &sub_str[..i];
+                val = &sub_str[i+1..];
+            }
+
+            // if the key exists already
+            data
+            .entry(key)
+            .and_modify(|existing: &mut Value| match existing{
+                Value::Single(prev_val) => {
+                    //let mut vec = vec![prev_val, val];
+                    *existing = Value::Multiple(vec![prev_val, val]);
+                }
+                Value::Multiple(vec) => vec.push(val)
+            })
+            .or_insert(Value::Single(val));
+        }
+
+        QueryString {data}
+    }
+}
