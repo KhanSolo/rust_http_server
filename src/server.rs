ea367b8c7efb4a06311233f01f460871c6ef22c0
Konstantin Ta <drpdrp@mail.ru>
2022-01-09 23:36:38 +0300

    CHG: implementing ThreadPool

--- src/server.rs
+++ src/server.rs
@@ -3,12 +3,12 @@ use rust_http_server::ThreadPool;
 use crate::http::{ParseError, Request, Response, StatusCode};
 
 use std::convert::TryFrom;
-use std::io::Read;
-use std::net::TcpListener;
+use std::io::{Error, Read};
+use std::net::{SocketAddr, TcpListener, TcpStream};
 
 pub trait Handler {
-    fn handle_request(&mut self, request: &Request) -> Response;
-    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+    fn handle_request(&self, request: &Request) -> Response;
+    fn handle_bad_request(&self, e: &ParseError) -> Response {
         print!("Failed to parse request: {}", e);
         Response::new(StatusCode::BadRequest, None)
     }
@@ -23,41 +23,49 @@ impl Server {
         Self { addr }
     }
 
-    pub fn run(self, mut handler: impl Handler) {
+    pub fn run(self, handler: impl Handler + Send+ std::marker::Sync) {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
         let pool = ThreadPool::new(4);
 
         loop {
-            match listener.accept() {                
-                Ok((mut stream, addr)) => {
-                    let mut buffer = [0; 1024];
-                    match stream.read(&mut buffer) {
-                        Ok(bytes_count) => {
-                            println!(
-                                "Received {} bytes of a request: {}",
-                                bytes_count,
-                                String::from_utf8_lossy(&buffer)
-                            );
-
-                            let response = match Request::try_from(&buffer[..]) {
-                                Ok(request) => handler.handle_request(&request),
-                                Err(e) => handler.handle_bad_request(&e),
-                            };
-                            if let Err(e) = response.send(&mut stream) {
-                                println!("Failed to send response {}", e);
-                            }
-                        }
-                        Err(e) => {
-                            println!("Failed to read from connecion {}", e);
-                        }
-                    };
-                }
-                Err(e) => {
-                    println!("Failed to establish a connection: {}", e);
-                }
-            }
+            let result: Result<(TcpStream, SocketAddr), Error> = listener.accept();
+            let r = &handler;
+            pool.execute(||{
+                process_stream(result, r);
+            });            
         }
     }
 }
+    fn process_stream(result: Result<(TcpStream, SocketAddr), Error>, handler: &impl Handler) {
+        match result {                
+            Ok((mut stream, _)) => {
+                let mut buffer = [0; 1024];
+                match stream.read(&mut buffer) {
+                    Ok(bytes_count) => {
+                        println!(
+                            "Received {} bytes of a request: {}",
+                            bytes_count,
+                            String::from_utf8_lossy(&buffer)
+                        );
+
+                        let response = match Request::try_from(&buffer[..]) {
+                            Ok(request) => handler.handle_request(&request),
+                            Err(e) => handler.handle_bad_request(&e),
+                        };
+                        if let Err(e) = response.send(&mut stream) {
+                            println!("Failed to send response {}", e);
+                        }
+                    }
+                    Err(e) => {
+                        println!("Failed to read from connecion {}", e);
+                    }
+                };
+            }
+            Err(e) => {
+                println!("Failed to establish a connection: {}", e);
+            }
+        }
+    }
+
